cfa3a75b63c9e65787856db4ecd69b73bce33853
kirisaki-vk <kirisaki.vk@gmail.com>
2025-01-13 22:58:46 +0300

    feat: add public path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('public')
+  getPublic() {
+    return 'This endpoint is public and can be called unauthenticated';
+  }
 }
